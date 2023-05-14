c4a7e5e02cb55581aed9633bd32a7c068cf22c40
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2023-05-14 19:53:55 +0530

    c program
    calculate simple interest

---
+++ simple interest.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    float p,n,r,SI=0.0;
+    
+    p=1000.0;
+    n=2.5;
+    r=3.1;
+    SI=(p*r*n)/100;
+    printf("simple interest is:%f",SI)
+    getch();
+}
