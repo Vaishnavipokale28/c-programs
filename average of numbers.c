9f131ac31d25319d7b292c989bf37689627afc08
Vaishnavipokale28 <vaishnavipokale069@gmail.com>
2023-05-14 20:31:37 +0530

    c program
    calculate average of numbers

---
+++ average of numbers.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    float n1,n2,n3,n4,n5;
+    printf("\n Enter 5 numbers");
+    scanf("%f,%f,%f,%f,%f",&n1,&n2,&n3,&n4,&n5);
+    printf("\n Average is: %f",(n1+n2+n3+n4+n5)/5);
+    getch();
+}
